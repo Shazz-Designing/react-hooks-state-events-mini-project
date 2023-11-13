74c776777e563a0012a3629f2691672298a6ce59
sharon <sapot@safaricom.co.ke>
2023-11-13 21:55:43 +0300

    update the NewTaskForm component

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState(categories[0]); // Initialize with the first category
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Check if the entered text is not empty
+    if (text.trim() === "") {
+      alert("Please enter a task!");
+      return;
+    }
+
+    // Create a new task object
+    const newTask = {
+      text: text,
+      category: category,
+    };
+
+    // Callback prop to submit the new task
+    onTaskFormSubmit(newTask);
+
+    // Reset form state
+    setText("");
+    setCategory(categories[0]);
+  };
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={text} onChange={handleTextChange} />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" value={category} onChange={handleCategoryChange}>
+          {categories.map((cat) => (
+            <option key={cat} value={cat}>
+              {cat}
+            </option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
