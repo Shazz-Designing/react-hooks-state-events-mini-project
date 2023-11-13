28509ee511cdc520f55509ee3526b907dec84c8e
sharon <sapot@safaricom.co.ke>
2023-11-13 21:37:04 +0300

    Call the onDelete callback

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-function Task({ category, text, onDelete }) {
+function Task({ text, category, onDelete }) {
+  const handleDeleteClick = () => {
+    if (onDelete) {
+      onDelete();
+    }
+  };
+
   return (
     <div className="task">
       <div className="label">{category}</div>
       <div className="text">{text}</div>
-      <button className="delete" onClick={onDelete}>
+      <button className="delete" onClick={handleDeleteClick}>
         X
       </button>
     </div>
